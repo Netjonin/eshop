96e6c8bef3979a712d89d15370b754fa26fefb2f
netjonin <pencilpepeyo@yahoo.com>
2024-01-28 14:14:48 +0100

    pagination functionality added

---
+++ eshop-frontend/src/app/components/public/item-details/item-details.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ItemService } from '../../../services/item/item.service';
+import { Item } from '../../../models/item';
+
+
+@Component({
+  selector: 'app-item-details',
+  templateUrl: './item-details.component.html',
+  styleUrl: './item-details.component.css'
+})
+export class ItemDetailsComponent implements OnInit {
+
+  item: Item = { id: 0, name: "", price: 0, category: "", description: "" };
+  constructor(
+    private route: ActivatedRoute,
+    private itemService: ItemService
+  ) { }
+  ngOnInit(): void {
+    this.getItem();
+  }
+  getItem() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (!isNaN(id)) {
+      this.itemService.getItem(id)
+        .subscribe(item => this.item = item);
+    }
+  }
+  addToCart(): void { }
+}
+
+
